d2f3436a43a97eff176c1923106013661b80e70b
arifjokam <arif.hidayatulloh313@gmail.com>
2020-08-07 17:40:20 +0700

    Upload Projek Vue

---
+++ README.md
@@ -0,0 +1,29 @@
+<<<<<<< HEAD
+# shayna
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
+=======
+# Ubstore
+Projek aplikasi Vue sebagai bahan belajar kelas Full-Stack Laravel VueJS (Toko Online)
+>>>>>>> af9a587908e74997f8e91a6cee9c3c956d0c8459
---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <HeaderShayna />
+    <HeroUb1 />
+    <HeroUb2 />
+    <InstaUb />
+    <LogoPatners />
+    <FooterUb />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HeaderShayna from "@/components/HeaderShayna.vue";
+import HeroUb1 from "@/components/HeroUb1.vue";
+import HeroUb2 from "@/components/HeroUb2.vue";
+import InstaUb from "@/components/InstaUb.vue";
+import LogoPatners from "@/components/LogoPatners.vue";
+import FooterUb from "@/components/FooterUb.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HeaderShayna,
+    HeroUb1,
+    HeroUb2,
+    InstaUb,
+    LogoPatners,
+    FooterUb,
+  },
+};
+</script>
